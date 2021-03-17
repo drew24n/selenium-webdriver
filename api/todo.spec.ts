4d6bafdc9b7d9b88d7c704711d8eeb2bcf45ab33
Admin <drew24n@gmail.com>
2021-03-17 23:22:59 +0200

    api setup configuration

---
+++ api/todo.spec.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+import { expect } from 'chai';
+
+it('check todo item values', async function () {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+  expect(res.data).to.deep.equal({
+    userId: 1,
+    id: 1,
+    title: 'delectus aut autem',
+    completed: false
+  });
+});
